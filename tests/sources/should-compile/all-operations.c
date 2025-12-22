1b11637bd0e94212040032d9ae26646d6b5c34be
AbdelrahmanMohamed-Fathy <abdelrahman.fathy04@outlook.com>
2025-12-22 16:50:50 +0000

    added all other operations

--- tests/sources/should-compile/all-operations.c
+++ tests/sources/should-compile/all-operations.c
@@ -7,7 +7,9 @@ int y = 2;
     x = x / 3;
     y = x % 2;
     x = x + 1;
-
+    x ^ y;
+    x = -1;
+    
     // logical operations to be supported:
     x && y;
     x || y;
@@ -17,4 +19,5 @@ int y = 2;
     x >= y;
     x < y;
     x <= y;
+    !(x == y); 
 }
