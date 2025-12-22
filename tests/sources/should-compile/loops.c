7e0380c1a12ca65a0957a0e2a88d1c1ba67f227f
Mxdness <abdelrahman.amsnour04@eng-st.cu.edu.eg>
2025-12-22 17:25:32 +0000

    - Fixed For loops, scopes didnt properly delete previous initalizations after exiting the scope

--- tests/sources/should-compile/loops.c
+++ tests/sources/should-compile/loops.c
@@ -1,32 +1,26 @@
 int x = 10;
 int y = 0;
 
-for (int i = 0; i < 10; i++) {
+for (int i = 0; i < 10; i = i+1) {
     x = x + i;
 }
 
-for (int i = 0; i < 10; i++)  x = x + i;
+for (int i = 0; i < 10; i = i+1)  x = x + i;
 
-for (int i = 0; i < 3; i++);
+for (int i = 0; i < 3; i = i+1);
 
 while (x < 10){ 
-    x++; 
+    x = x + 1; 
 }
 
-while (y < 5) y++;
+while (y < 5) y = y + 1;
 
 while (x < 5);
 
-do{x++;} while (x < 5);
-
-do y++; while (y < 5);
-
-do; while (y < 5);
-
 repeat {
-     x++; 
+     x = x + 1; 
     } until(x > 10);
 
-repeat y++; until(y > 10);
+repeat y = y + 1; until(y > 10);
 
 repeat; until(y > 10);
